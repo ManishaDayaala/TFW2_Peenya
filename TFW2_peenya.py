676eed4db239bf16300362b4a815536609bd648f
ManishaDayaala <Manishadayaala@gmail.com>
2025-04-14 17:23:28 +0530

    Update TFW2_peenya.py

--- TFW2_peenya.py
+++ TFW2_peenya.py
@@ -26,7 +26,7 @@ def set_random_seed(seed_value=42):
     tf.random.set_seed(seed_value)
 
 # Define the main folder path
-MAINFOLDER = r"D:\APPdata_TFW2peenya"
+MAINFOLDER = r"./APPdata_TFW2peenya"
 
 # Create other paths relative to the main folder
 training_file_path = os.path.join(MAINFOLDER, "Training", "Training_TFW2peenya.xlsx")  # FIXED TRAINING DATA
